cab4d22b3c399e5f16677ab975d714f759ee97cc
guigx <guigo211103@gmail.com>
2024-02-07 11:23:10 -0300

    Requisições DELETE

--- api/src/main/java/med/voll/api/controller/MedicoController.java
+++ api/src/main/java/med/voll/api/controller/MedicoController.java
@@ -36,6 +36,12 @@ public class MedicoController {
         medico.atualizarInformacoes(dados);
     }
 
+    @DeleteMapping("/{id}")
+    @Transactional
+    public void excluir(@PathVariable Long id){
+        repository.deleteById(id);
+    }
+
 }
 
 
